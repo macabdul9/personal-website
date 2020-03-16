bedc2285a98f56735615a0035a7a413a046f29be
macabdul9 <abdulwaheed1513@gmail.com>
2020-03-17 00:27:32 +0530

    resume is updated

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,15 +1,16 @@
 const degrees = [
   {
-    school: 'Stanford University',
-    degree: 'M.S. Computational and Mathematical Engineering',
-    link: 'https://stanford.edu',
-    year: 2016,
-  }, {
-    school: 'University at Buffalo',
-    degree: 'B.S. Electrical Engineering, Computer Engineering',
-    link: 'https://buffalo.edu',
-    year: 2012,
-  },
+    school: 'Maharaja Agrasen Institute of Technology, New Delhi, India.',
+    degree: 'B.Tech Computer Science and Engnineering',
+    link: 'https://cse.mait.ac.in/',
+    year: "Expected - 2021",
+  }, 
+  // {
+  //   school: 'University at Buffalo',
+  //   degree: 'B.S. Electrical Engineering, Computer Engineering',
+  //   link: 'https://buffalo.edu',
+  //   year: 2012,
+  // },
 ];
 
 export default degrees;
